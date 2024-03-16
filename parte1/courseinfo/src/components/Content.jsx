714c2af0beafa6d6deca1479bd1723ee4014e65d
Yarlenis <122038662+yarle88@users.noreply.github.com>
2024-03-16 19:25:55 -0400

    1.2

--- parte1/courseinfo/src/components/Content.jsx
+++ parte1/courseinfo/src/components/Content.jsx
@@ -1,15 +1,11 @@
+import Part from "./Part"
+
 export default function Content({part1, part2, part3, exercises1, exercises2, exercises3}){
     return(
         <>
-         <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+        <Part part={part1} exercises={exercises1}/>
+        <Part part={part2} exercises={exercises2}/>
+        <Part part={part3} exercises={exercises3}/>
         </>
     );
 }
