5bf98fd71a9c325357139e083414961d9cb08bf9
Yarlenis <122038662+yarle88@users.noreply.github.com>
2024-03-16 19:21:00 -0400

    1.1

---
+++ parte1/courseinfo/src/components/Content.jsx
@@ -0,0 +1,15 @@
+export default function Content({part1, part2, part3, exercises1, exercises2, exercises3}){
+    return(
+        <>
+         <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+        </>
+    );
+}
---
+++ parte1/courseinfo/src/components/Total.jsx
@@ -0,0 +1,5 @@
+export default function Total({exercises1, exercises2, exercises3}){
+    return(
+        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    );
+}
