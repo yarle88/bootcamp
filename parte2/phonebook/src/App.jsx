b60b8fe6eaa19cf4b20c31e38844dcb4c877595d
Yarlenis <122038662+yarle88@users.noreply.github.com>
2024-03-18 18:23:58 -0400

    2.9

--- parte2/phonebook/src/App.jsx
+++ parte2/phonebook/src/App.jsx
@@ -1,62 +1,68 @@
-import { useState } from 'react'
-import './App.css'
-import Person from './components/Person'
+import { useState } from "react";
+import "./App.css";
+import Person from "./components/Person";
+import { personsData } from "./data";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-     phone:'111111111'
-   }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [phone, setPhone]=useState('');
-  
-  const addPerson=(e)=>{
+  const [persons, setPersons] = useState(personsData);
+  const [newName, setNewName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [filterItem, setFilterItem] = useState("");
+
+  const list=filterItem!=''? persons.filter(person=>person.name.toLowerCase().includes(filterItem)):persons;
+
+  const addPerson = (e) => {
     e.preventDefault();
-    const newPerson={
-      name:newName,
-      phone:phone
+    const newPerson = {
+      name: newName,
+      phone: phone,
     };
-    const index=persons.findIndex(person=>person.name==newName )
+    const index = persons.findIndex((person) => person.name == newName);
 
-    if(index==-1){
+    if (index == -1) {
       setPersons(persons.concat(newPerson));
+    } else {
+      alert(`${newName} is already added to the phonebook`);
     }
-    else{
-      alert(`${newName} is already added to the phonebook`)
-    }
-   
-  }
+  };
 
-  const changeName=(e)=>{
-    console.log('name :>> ', newName);
+  const changeName = (e) => {
+    console.log("name :>> ", newName);
     setNewName(e.target.value);
-  }
-  const changePhone=(e)=>{
+  };
+  const changePhone = (e) => {
     setPhone(e.target.value);
-  }
-
+  };
+  const changeFilter = (e) => {
+    setFilterItem(e.target.value.toLowerCase());
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div className="filter">
+      <p className="filter-text">Filter shown with: </p>
+      <input type="text" className="filter-input" onChange={changeFilter} value={filterItem} />
+      </div>
       <form onSubmit={addPerson}>
         <div>
-          Name: <input onChange={changeName} value={newName}/>
+          Name: <input onChange={changeName} value={newName} />
         </div>
         <div>
-          Phone: <input onChange={changePhone} value={phone}/>
+          Phone: <input onChange={changePhone} value={phone} />
         </div>
         <div>
-          <button className='btn' type="submit">Add</button>
+          <button className="btn" type="submit">
+            Add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-     { persons.map(person=>
-     <Person key={person.name} person={person}/>
-    )}
+      {list.map((person) => (
+        <Person key={person.name} person={person} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
