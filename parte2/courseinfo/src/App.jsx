5f2f3468b7bcbe1fdcd59bb5c95e8c6651787b60
Yarlenis <122038662+yarle88@users.noreply.github.com>
2024-03-18 12:36:01 -0400

    2.4

--- parte2/courseinfo/src/App.jsx
+++ parte2/courseinfo/src/App.jsx
@@ -1,29 +1,32 @@
-import Course from "./components/Course "
-
+import Course from "./components/Course ";
+import { courses } from "./data";
 
 const App = () => {
   const course = {
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10
+        name: "Fundamentals of React",
+        exercises: 10,
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7
+        name: "Using props to pass data",
+        exercises: 7,
       },
       {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
-    <Course course={course}/>
-  )
-}
+    <>
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
+    </>
+  );
+};
 
-export default App
+export default App;
