37cc7f32289c8e8836e879aa708032f6db58aff8
Yarlenis <122038662+yarle88@users.noreply.github.com>
2024-03-18 17:50:52 -0400

    2.8

--- parte2/phonebook/src/App.jsx
+++ parte2/phonebook/src/App.jsx
@@ -1,19 +1,24 @@
 import { useState } from 'react'
+import './App.css'
+import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+     phone:'111111111'
+   }
   ]) 
   const [newName, setNewName] = useState('')
-
+  const [phone, setPhone]=useState('');
+  
   const addPerson=(e)=>{
     e.preventDefault();
-    const newPerson={name:newName};
+    const newPerson={
+      name:newName,
+      phone:phone
+    };
     const index=persons.findIndex(person=>person.name==newName )
 
-    console.log(newPerson)
-    console.log("persons: ",persons)
-    console.log('index :>> ', index);
     if(index==-1){
       setPersons(persons.concat(newPerson));
     }
@@ -24,22 +29,32 @@ const App = () => {
   }
 
   const changeName=(e)=>{
+    console.log('name :>> ', newName);
     setNewName(e.target.value);
   }
+  const changePhone=(e)=>{
+    setPhone(e.target.value);
+  }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input onChange={changeName} value={newName}/>
+          Name: <input onChange={changeName} value={newName}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          Phone: <input onChange={changePhone} value={phone}/>
+        </div>
+        <div>
+          <button className='btn' type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-     { persons.map(person=><p key={person.name}>{person.name}</p>)}
+     { persons.map(person=>
+     <Person key={person.name} person={person}/>
+    )}
     </div>
   )
 }
---
+++ parte2/phonebook/src/components/Person.jsx
@@ -0,0 +1,5 @@
+export default function Person({person}){
+return(
+<p>{person.name}  {person.phone}</p>
+)
+}
